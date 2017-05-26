50284e07cd4bf0f5cde474eb262880ab2d7e334a
SPI\SValenzuela <svalenzuela@spigroup.com>
2017-05-26 14:58:24 -0400

    Committing App.js file.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var i = 2;
+    var myStyle = {
+      fontSize: 20,
+      color: '#00ffff'
+    };
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +18,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h4 style={myStyle}>Hello World!</h4>
+        <p>{1+1}</p>
+        <p>{i == 1 ? "True" : "False"}</p>
       </div>
     );
   }
